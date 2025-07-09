e865b534b98e3d0b2b39a53a899a3ac36e2b513c
dravivar <dravivar@asu.edu>
2025-07-08 21:43:50 -0700

    fix: correct logo path

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -54,7 +54,7 @@ const EventRSVPCard = () => {
 
         {/* Static Local Image */}
         <img
-          src="/src/images/Tempe.jpg  " // 🔁 Put this image inside public/images/
+          src="/public/Tempe.jpg  " // 🔁 Put this image inside public/images/
           alt="Tempe, Arizona"
           className="rounded-xl mb-4 w-full max-h-48 object-cover"
         />
